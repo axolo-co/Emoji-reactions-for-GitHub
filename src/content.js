e792a465a177a34185fc8cd6c50a7162709d7214
Sydney Cohen <chnsydney@gmail.com>
2023-12-21 17:58:51 +0100

    refactor some code

--- src/content.js
+++ src/content.js
@@ -13,20 +13,6 @@ function addEmojisToCodeLines() {
         // Add styles to emojiContainer for positioning and styling
         emojiContainer.classList.add('emoji-container')
 
-        const emojis = [
-          { emoji: '👍', tooltip: 'I like this' },
-          { emoji: '🔧', tooltip: 'Needs to be changed' },
-          { emoji: '❓', tooltip: 'I have a question' },
-          { emoji: '🤔', tooltip: 'Thinking aloud or suggesting alternatives' },
-          { emoji: '🌱', tooltip: 'Planting a seed for future' },
-          { emoji: '📝', tooltip: 'Explanatory note' },
-          { emoji: '⛏️', tooltip: 'Nitpick' },
-          { emoji: '♻️', tooltip: 'Suggestion for refactoring' },
-          { emoji: '🏕', tooltip: 'Opportunity to improve the codebase' },
-          { emoji: '📌', tooltip: 'Concerns out of scope' },
-          { emoji: '💡', tooltip: 'A new idea or suggestion' },
-        ]
-
         emojis.forEach(({ emoji, tooltip }) => {
           const emojiButton = document.createElement('button')
           emojiButton.innerText = emoji
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+const emojis = [
+  { emoji: '👍', tooltip: 'I like this' },
+  { emoji: '🔧', tooltip: 'Needs to be changed' },
+  { emoji: '❓', tooltip: 'I have a question' },
+  { emoji: '🤔', tooltip: 'Thinking aloud or suggesting alternatives' },
+  { emoji: '🌱', tooltip: 'Planting a seed for future' },
+  { emoji: '📝', tooltip: 'Explanatory note' },
+  { emoji: '⛏️', tooltip: 'Nitpick' },
+  { emoji: '♻️', tooltip: 'Suggestion for refactoring' },
+  { emoji: '🏕', tooltip: 'Opportunity to improve the codebase' },
+  { emoji: '📌', tooltip: 'Concerns out of scope' },
+  { emoji: '💡', tooltip: 'A new idea or suggestion' },
+]
